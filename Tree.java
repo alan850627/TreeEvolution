19cbdba295796f3a132870eeddd61bd10a6fbc75
alan850627 <alan850627@gmail.com>
2016-09-13 16:39:53 -0500

    Fix the structure a bit

---
+++ Tree.java
@@ -0,0 +1,280 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+
+public class Tree {
+
+	private static final int WIDTH_VARIATION = 5; // how many grids each sector
+													// the gene represents.
+	private static final int HEIGHT_VARIATION = 5; // how many grids each
+													// sector the gene
+													// represents.
+	private static final int GENE_TOTAL = 10; // must be multiple of 5.
+	private static final int COLOR_CHANGE_TIME = 10; // number of frames before
+														// a color changes
+	private static final int INITIAL_HEALTH = 1000;// not sure if I want to use
+													// this.
+	private static final int OLD_AGE_FACTOR = 1000; // the bigger the number,
+													// the slower it'll die.
+
+	public int[] width_gene = { 2, 2, 2, 2, 2 }; // must add up to
+													// GENE_TOTAL
+	public int[] height_gene = { 2, 2, 2, 2, 2 }; // must add up to
+														// GENE_TOTAL
+
+	public int max_branch_per_node = 2;
+	public int reproduce_time = 1000;
+	public int grow_time = 100;
+	public int health = 1000;
+	public int leaf_size = 1;
+
+	public int amount_of_tree = 1;
+	public int age = 1;
+	public int generation = 1;
+	public int color = 255;
+
+	BranchNode root;
+
+	public Tree() {
+		root = new BranchNode();
+	}
+
+	public Tree(int x, int y, Grid[][] map) {
+		root = new BranchNode(x, y);
+		root.addLeaf(this, map);
+	}
+
+	public Tree(Grid[][] map) {
+		root = new BranchNode();
+		root.addLeaf(this, map);
+	}
+
+	// Reproduce from parent
+	public Tree(Tree parent, int x, int y, Grid[][] map) {
+		// mutate!
+		height_gene = parent.height_gene.clone();
+		if (World.BRANCH_HEIGHT_MUTATION) {
+			int r = (int) (Math.random() * 5);
+			height_gene[r] += 1;
+			r = (int) (Math.random() * 5);
+			while (height_gene[r] == 0) {
+				r = (int) (Math.random() * 5);
+			}
+			height_gene[r] -= 1;
+		}
+
+		width_gene = parent.width_gene.clone();
+		if (World.BRANCH_WIDTH_MUTATION) {
+			int r = (int) (Math.random() * 5);
+			width_gene[r] += 1;
+			r = (int) (Math.random() * 5);
+			while (width_gene[r] == 0) {
+				r = (int) (Math.random() * 5);
+			}
+			width_gene[r] -= 1;
+		}
+
+		reproduce_time = parent.reproduce_time;
+		if (World.REPRODUCE_TIME_MUTATION) {
+			int r = (int) (Math.random() * 11) - 5;
+			if (reproduce_time + r >= 0) {
+				reproduce_time += r;
+			}
+		}
+
+		grow_time = parent.grow_time;
+		if (World.GROW_TIME_MUTATION) {
+			int r = (int) (Math.random() * 11) - 5;
+			if (grow_time + r >= 0) {
+				grow_time += r;
+			}
+		}
+
+		leaf_size = parent.leaf_size;
+		if (World.LEAF_SIZE_MUTATION) {
+			int r = (int) (Math.random() * 3) - 1;
+			if (leaf_size + r >= 0) {
+				leaf_size += r;
+			}
+		}
+
+		max_branch_per_node = parent.max_branch_per_node;
+		if (World.MAX_BRANCH_PER_NODE_MUTATION) {
+			int r = (int) (Math.random() * 3) - 1;
+			if (max_branch_per_node + r > 0) {
+				max_branch_per_node += r;
+			}
+		}
+
+		generation = parent.generation + 1;
+		health = INITIAL_HEALTH;
+		root = new BranchNode(x, y);
+
+		// make new leaf on new branch
+		root.addLeaf(this, map);
+	}
+
+	public void paintTree(Graphics2D g2, int i) {
+		// g2.setColor(new Color(139, 69, 19));
+		g2.setColor(new Color(color, color, color));
+
+		if (i == 0) {
+			g2.setColor(Color.RED);
+		}
+		paintBranch(g2, root);
+	}
+
+	private void paintBranch(Graphics2D g2, BranchNode bn) {
+		for (int i = 0; i < bn.children.size(); i += 1) {
+			paintBranch(g2, bn.children.get(i));
+		}
+
+		if (bn.parent != null) {
+			g2.drawLine(bn.x * World.SQUARE_SIZE, (World.WORLD_HEIGHT - bn.y)
+					* World.SQUARE_SIZE, bn.parent.x * World.SQUARE_SIZE,
+					(World.WORLD_HEIGHT - bn.parent.y) * World.SQUARE_SIZE);
+
+		}
+	}
+
+	public void processTree(Grid[][] map, ArrayList<Tree> trees) {
+		// if conditions meet, grow!
+		// if conditions meet, reproduce
+		// update health
+		// update age
+		// killing is done by World class.
+
+		// change color
+		if (age % COLOR_CHANGE_TIME == 0 && color > 0) {
+			color -= 1;
+		}
+
+		// /////////////I don't like it growing on timer////////////////////
+		if (age % grow_time == 0) {
+			grow(map);
+		}
+		if (age % reproduce_time == 0) {
+			reproduce(map, trees);
+		}
+
+		// update health based on how big the tree is, and how much light the
+		// leaves are getting
+		photosynthesis(root, map);
+		health -= amount_of_tree;
+		health -= age / OLD_AGE_FACTOR;
+
+		age += 1;
+	}
+
+	private void photosynthesis(BranchNode bn, Grid[][] map) {
+		if (bn.leaf_alive) {
+			for (int i = 0; i < leaf_size; i++) {
+				int l = bn.x + i - leaf_size / 2;
+				if (l >= 0 && l < World.WORLD_WIDTH) {
+					health += map[l][bn.y].sunlight;
+					map[l][bn.y].sunlight -= World.LEAF_BLOCK;
+				}
+			}
+		} else {
+			// If I really want to be safe, I should put a check for if bn.children.size() > 0
+			// But the for loop here checks it for me already.
+			for (int i = 0; i < bn.children.size(); i++) {
+				photosynthesis(bn.children.get(i), map);
+			}
+		}
+	}
+
+	public void reproduce(Grid[][] map, ArrayList<Tree> trees) {
+		for (int i = 0; i < health/age; i++) { // not sure how many children each tree
+										// should have...
+			BranchNode bn = root;
+			while (bn.children.size() > 0) {
+				bn = bn.children
+						.get((int) (Math.random() * bn.children.size()));
+			}
+			trees.add(new Tree(this, bn.x, 0, map));
+		}
+	}
+
+	public void kill(Grid[][] map) {
+		removeLeaves(map, root);
+	}
+
+	private void removeLeaves(Grid[][] map, BranchNode bn) {
+		if (bn.children.size() > 0) {
+			for (int i = 0; i < bn.children.size(); i++) {
+				removeLeaves(map, bn.children.get(i));
+			}
+		} else {
+			bn.killLeaf(this, map);
+		}
+	}
+
+	public void grow(Grid[][] map) {
+		// grows a new branch.
+		// check if any given node is full capacity. if not, grow or move to
+		// child.
+		// using node as reference, make a new node.
+		// update map as well
+
+		// pick an existing node to grow from.
+		BranchNode parent = root;
+		while (parent.children.size() > 0) {
+			if (parent.children.size() >= max_branch_per_node) {
+				// if no more room at this branch
+				parent = parent.children
+						.get((int) (Math.random() * parent.children.size()));
+			} else if (Math.random() < 0.5) {
+				// if there is still room, picks randomly
+				parent = parent.children
+						.get((int) (Math.random() * parent.children.size()));
+			} else {
+				break;
+			}
+		}
+
+		int newX, newY; // X and Y coordinates for the new node.
+		int r = (int) (Math.random() * GENE_TOTAL);
+		int gene_count = width_gene[0];
+		int width_select = 0;
+		while (r > gene_count) {
+			width_select += 1;
+			gene_count += width_gene[width_select];
+		}
+		r = (int) (Math.random() * WIDTH_VARIATION);
+		newX = parent.x - WIDTH_VARIATION / 2 + (width_select - 2)
+				* WIDTH_VARIATION + r;
+		if (newX < 0) {// if goes off the grid, push back.
+			newX = 0;
+		} else if (newX >= World.WORLD_WIDTH) {
+			newX = World.WORLD_WIDTH - 1;
+		}
+
+		r = (int) (Math.random() * GENE_TOTAL);
+		gene_count = height_gene[0];
+		int height_select = 0;
+		while (r > gene_count) {
+			height_select += 1;
+			gene_count += height_gene[height_select];
+		}
+		r = (int) (Math.random() * HEIGHT_VARIATION);
+		newY = parent.y + height_select * HEIGHT_VARIATION + r;
+		if (newY >= World.WORLD_HEIGHT) {
+			newY = World.WORLD_HEIGHT - 1;
+		}
+
+		// add the branch to the total body.
+		amount_of_tree += (int) Math.sqrt((newX - parent.x) * (newX - parent.x)
+				+ (newY - parent.y) * (newY - parent.y));
+		BranchNode child = new BranchNode(parent, newX, newY);
+		parent.children.add(child);
+
+		if (parent.leaf_alive) {
+			// kill leaf, because branch is growing off of it
+			parent.killLeaf(this, map);
+		}
+
+		// make new leaf on new branch
+		child.addLeaf(this, map);
+	}
+}
---
+++ World.java
@@ -0,0 +1,253 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class World extends JPanel implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	public static final int SCREEN_WIDTH = 1000;
+	public static final int SCREEN_HEIGHT = 1000;
+	public static final int SQUARE_SIZE = 5;
+	public static final int WORLD_WIDTH = SCREEN_WIDTH / SQUARE_SIZE;
+	public static final int WORLD_HEIGHT = SCREEN_HEIGHT / SQUARE_SIZE;
+	public static final int DELAY = 1;
+	public static final int SPEED_UP = 1;
+	public static final int INIT_NUM_TREES = 20; // numbers of trees in the
+													// initial state.
+
+	public static final int FULL_SUNLIGHT = 20;
+	public static final int LEAF_BLOCK = 5;
+
+	public static final boolean BRANCH_WIDTH_MUTATION = true;
+	public static final boolean BRANCH_HEIGHT_MUTATION = true;
+	public static final boolean REPRODUCE_TIME_MUTATION = true;
+	public static final boolean GROW_TIME_MUTATION = true;
+	public static final boolean LEAF_SIZE_MUTATION = true;
+	public static final boolean MAX_BRANCH_PER_NODE_MUTATION = true;
+
+	public static final int OBJ_EMPTY = 0;
+	public static final int OBJ_NODE = 1;
+	public static final int OBJ_LEAF = 2;
+
+	public int[] total_width_gene;
+	public int[] total_height_gene;
+	public int total_reproduce_time = 0;
+	public int total_grow_time = 0;
+	public int total_leaf_size = 0;
+	public int total_max_branch_per_node = 0;
+	public int total_generation = 0;
+	public int total_age = 0;
+	public int total_health = 0;
+	public int total_tree_size = 0;
+	public int time = 0;
+
+	public Timer timer;
+	public Grid[][] map = new Grid[WORLD_WIDTH][WORLD_HEIGHT]; // typical
+																// coordinate
+																// system, (0,0)
+																// at bottom
+																// left
+	public ArrayList<Tree> trees = new ArrayList<Tree>();
+	public Start parentClass;
+
+	public World(Start start) {
+		// TODO Auto-generated constructor stub
+		parentClass = start;
+		timer = new Timer(DELAY, this);
+		timer.start();
+
+		resetState();
+	}
+
+	public void resetState() {
+		for (int i = 0; i < WORLD_WIDTH; i++) {
+			for (int j = 0; j < WORLD_HEIGHT; j++) {
+				map[i][j] = new Grid();
+			}
+		}
+		trees.clear();
+		total_width_gene = new int[5];
+		total_height_gene = new int[5];
+		total_reproduce_time = 0;
+		total_grow_time = 0;
+		total_leaf_size = 0;
+		total_max_branch_per_node = 0;
+		total_generation = 0;
+		total_age = 0;
+		total_health = 0;
+		total_tree_size = 0;
+		time = 0;
+
+		// INITIAL STATE.
+		for (int i = 0; i < INIT_NUM_TREES; i++) {
+			trees.add(new Tree(WORLD_WIDTH / INIT_NUM_TREES * i, 0, map));
+		}
+	}
+
+	private void collectData(Tree t) {
+		for (int i = 0; i < 5; i++) {
+			total_width_gene[i] += t.width_gene[i];
+			total_height_gene[i] += t.height_gene[i];
+		}
+		total_reproduce_time += t.reproduce_time;
+		total_grow_time += t.grow_time;
+		total_leaf_size += t.leaf_size;
+		total_max_branch_per_node += t.max_branch_per_node;
+		total_generation += t.generation;
+		total_age += t.age;
+		total_health += t.health;
+		total_tree_size += t.amount_of_tree;
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		total_width_gene = new int[5];//
+		total_height_gene = new int[5];//
+		total_reproduce_time = 0;
+		total_grow_time = 0;
+		total_leaf_size = 0; //
+		total_max_branch_per_node = 0; //
+		total_generation = 0; //
+		total_age = 0; //
+		total_health = 0; //
+		total_tree_size = 0; //
+
+		for (int i = 0; i < trees.size(); i++) {
+			trees.get(i).processTree(map, trees);
+			collectData(trees.get(i));
+			if (trees.get(i).health <= 0) { // kill.
+				trees.get(i).kill(map);
+				trees.remove(i);
+				i--;
+			}
+		}
+
+		// Process Sunlight
+		for (int i = 0; i < WORLD_WIDTH; i++) {
+			int currentSunlight = FULL_SUNLIGHT;
+			for (int j = WORLD_HEIGHT - 1; j >= 0; j--) {
+				map[i][j].sunlight = currentSunlight;
+				if (map[i][j].type == OBJ_LEAF && currentSunlight > 0) {
+					currentSunlight -= LEAF_BLOCK;
+				}
+			}
+		}
+
+		time += 1;
+	}
+
+	public void paint(Graphics g) {
+		if (time % SPEED_UP == 0) {
+			super.paintComponent(g);
+			Graphics2D g2 = (Graphics2D) g;
+
+			// ////////////TURN ON SUNLIGHT HERE////////////////
+			g2.setStroke(new BasicStroke(SQUARE_SIZE));
+			paintSunlight(g2);
+			// /////////////////////////////////////////////////
+
+			// Draws all the branches of the trees
+			g2.setStroke(new BasicStroke(SQUARE_SIZE / 2));
+			for (int i = 0; i < trees.size(); i++) {
+				trees.get(i).paintTree(g2, i);
+			}
+
+			g2.setStroke(new BasicStroke(SQUARE_SIZE));
+			for (int i = 0; i < WORLD_WIDTH; i++) {
+				for (int j = 0; j < WORLD_HEIGHT; j++) {
+					if (map[i][j].type == OBJ_LEAF) {
+						g2.setColor(Color.GREEN);
+						g2.drawLine(i * SQUARE_SIZE, (WORLD_HEIGHT - j)
+								* SQUARE_SIZE, i * SQUARE_SIZE,
+								(WORLD_HEIGHT - j) * SQUARE_SIZE);
+					}
+				}
+			}
+
+			// ///////////////////////PRINT STATS////////////////
+			if (trees.size() > 0) {
+				g2.setFont(new Font("TimesRoman", Font.BOLD, 15));
+				g2.setColor(Color.BLACK);
+				g2.drawString("AVERAGES:", SCREEN_WIDTH, 20);
+				g2.setFont(new Font("TimesRoman", Font.PLAIN, 15));				
+				g2.drawString("GENERATION: "
+						+ (total_generation / trees.size()), SCREEN_WIDTH, 40);
+				g2.drawString("AGE: " + (total_age / trees.size()),
+						SCREEN_WIDTH, 60);
+				g2.drawString("SIZE: " + (total_tree_size / trees.size()),
+						SCREEN_WIDTH, 80);
+				g2.drawString("HEALTH: " + (total_health / trees.size()),
+						SCREEN_WIDTH, 100);
+				g2.drawString("WIDTH GENE: ", SCREEN_WIDTH, 120);
+				g2.drawString("HEIGHT GENE: ", SCREEN_WIDTH, 140);
+				for (int i = 0; i < 5; i++) {
+					g2.drawString("" + (total_width_gene[i] / trees.size())
+							+ ", ", SCREEN_WIDTH + 110 + i * 20, 120);
+					g2.drawString("" + (total_height_gene[i] / trees.size())
+							+ ", ", SCREEN_WIDTH + 110 + i * 20, 140);
+				}
+				g2.drawString("LEAF SIZE: " + (total_leaf_size / trees.size()),
+						SCREEN_WIDTH, 160);
+				g2.drawString("MAX BRANCH PER NODE: "
+						+ (total_max_branch_per_node / trees.size()),
+						SCREEN_WIDTH, 180);
+				g2.drawString("GROW TIME: " + (total_grow_time / trees.size()),
+						SCREEN_WIDTH, 200);
+				g2.drawString("REPRODUCE TIME: "
+						+ (total_reproduce_time / trees.size()), SCREEN_WIDTH,
+						220);
+
+				// /////INFO FOR RED TREE//////////
+				g2.setFont(new Font("TimesRoman", Font.BOLD, 15));
+				g2.setColor(Color.RED);
+				g2.drawString("RED TREE:", SCREEN_WIDTH, 300);
+				g2.setFont(new Font("TimesRoman", Font.PLAIN, 15));
+				Tree t = trees.get(0);
+				g2.drawString("GENERATION: " + t.generation, SCREEN_WIDTH, 320);
+				g2.drawString("AGE: " + t.age, SCREEN_WIDTH, 340);
+				g2.drawString("SIZE: " + t.amount_of_tree, SCREEN_WIDTH, 360);
+				g2.drawString("HEALTH: " + t.health, SCREEN_WIDTH, 380);
+				g2.drawString("WIDTH GENE: ", SCREEN_WIDTH, 400);
+				g2.drawString("HEIGHT GENE: ", SCREEN_WIDTH, 420);
+				for (int i = 0; i < 5; i++) {
+					g2.drawString("" + (t.width_gene[i]) + ", ", SCREEN_WIDTH
+							+ 110 + i * 20, 400);
+					g2.drawString("" + (t.height_gene[i]) + ", ", SCREEN_WIDTH
+							+ 110 + i * 20, 420);
+				}
+				g2.drawString("LEAF SIZE: " + t.leaf_size, SCREEN_WIDTH, 440);
+				g2.drawString("MAX BRANCH PER NODE: " + t.max_branch_per_node,
+						SCREEN_WIDTH, 460);
+				g2.drawString("GROW TIME: " + t.grow_time, SCREEN_WIDTH, 480);
+				g2.drawString("REPRODUCE TIME: " + t.reproduce_time,
+						SCREEN_WIDTH, 500);
+				g2.setFont(new Font("TimesRoman", Font.BOLD, 15));
+				g2.setColor(Color.BLACK);
+				g2.drawString("TIME: " + time, SCREEN_WIDTH, 600);
+				g2.drawString("POPULATION: " + trees.size(), SCREEN_WIDTH, 620);
+			}
+		}
+		repaint();
+
+	}
+
+	private void paintSunlight(Graphics2D g2) {
+		int temp;
+		for (int i = 0; i < WORLD_WIDTH; i++) {
+			for (int j = 0; j < WORLD_HEIGHT; j++) {
+				temp = (255 * map[i][j].sunlight) / FULL_SUNLIGHT;
+				g2.setColor(new Color(temp, temp, 0));
+				g2.drawLine(i * SQUARE_SIZE, (WORLD_HEIGHT - j) * SQUARE_SIZE,
+						i * SQUARE_SIZE, (WORLD_HEIGHT - j) * SQUARE_SIZE);
+			}
+		}
+	}
+
+}
